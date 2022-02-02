a2513231a2486e0e9ccd3830aad5b580ca2648c8
paulcute99 <paulcuteflorin9@gmail.com>
2022-02-02 18:09:45 +0200

    Tesla-Clone

---
+++ src/components/Header.js
@@ -0,0 +1,121 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    top:0;
+    left:0;
+    right: 0;
+    justify-content: space-between;
+    z-index: 1;
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: center;
+
+    p{
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`
+
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+    a{
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+    }
+`
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 100;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s;
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0,0,0, .2);
+
+        a{
+            font-weight: 600;
+        }
+    }
+`
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+
+`
+
+function Header() {
+
+    const [burgerStatus, setBurguerStatus]  = useState(false);
+    
+
+    return (
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model X</a></p>
+                <p><a href="#">Model Y</a></p>
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <CustomMenu onClick={() => setBurguerStatus(true)}/>
+            </RightMenu>
+            <BurgerNav show = {burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurguerStatus(false)}/>
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Model Y</a></li>
+                <li><a href="#">Roadster</a></li>
+            </BurgerNav>
+        </Container>
+    )
+}
+
+export default Header
